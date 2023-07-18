e496d7c61f4778b64186722ca619b2f3ea8fd18a
Emre kavcakar <kavcakaremre@gmail.com>
2023-07-18 16:16:20 -0400

    try catch

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,23 +1,24 @@
-
+import axios from "axios";
+import { useEffect } from "react";
 import './App.css';
 
 function App() {
+  const key ="a90e03dfd6907f6b2ad98a5503fc568e"
+  useEffect(() => {
+    async function getApi() {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=15220,pittsburg&appid=${key}`);
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getApi();
+  },[])   
+
   return (
     <div className="App">
-      <header className="App-header">
-    
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       Hello
     </div>
   );
 }
