986ddbf03a04a9c3602f6292c9db70640cfaffb3
Emre kavcakar <kavcakaremre@gmail.com>
2023-07-18 16:31:23 -0400

    input

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,24 +1,30 @@
 import axios from "axios";
-import { useEffect } from "react";
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const key ="a90e03dfd6907f6b2ad98a5503fc568e"
+  const key = "a90e03dfd6907f6b2ad98a5503fc568e";
+  const [city, setCity] = useState();
   useEffect(() => {
     async function getApi() {
       try {
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=15220,pittsburg&appid=${key}`);
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${key}&units=metric`
+        );
         console.log(response);
+        setCity(response.data);
       } catch (error) {
         console.error(error);
       }
     }
     getApi();
-  },[])   
+  }, []);
 
   return (
     <div className="App">
-       Hello
+      <div>
+        <input className="border-8" type="text" />
+      </div>
     </div>
   );
 }
